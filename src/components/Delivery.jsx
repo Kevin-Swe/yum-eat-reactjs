90f8739dd0a293453f031d9bf5807fb901c5ffd7
Kevin Swe <yegaungswe.2080@gmail.com>
2023-08-24 12:09:21 +0630

    Update Delivery.jsx

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -7,7 +7,7 @@ const Delivery = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Deliery App</h3>
-        <div className='w-[1520px] mx-auto grid md:grid-cols-2'>
+        <div className='w-[1400px] mx-auto grid md:grid-cols-2'>
             <img className='w-[550px] mx-auto my-4' 
             src="https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672076/NetflixApp/FC_two_phones.6ec9a842f905769677f9_m91off.webp" alt="" />
             <div className='flex flex-col justify-center'>
